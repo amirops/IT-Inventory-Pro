06120288cee9f9fba4e257802debe2739b21dc47
Amir Bahmanabadi <amirbahmanabadi@outlook.com>
2026-07-18 16:05:24 +0330

    Add Get-SystemInfo Function

--- Modules/SystemInfo.psm1
+++ Modules/SystemInfo.psm1
@@ -1,1 +1,27 @@
-# System Information Module
+function Get-SystemInfo {
+
+    $os = Get-CimInstance Win32_OperatingSystem
+    $computer = Get-CimInstance Win32_ComputerSystem
+    $bios = Get-CimInstance Win32_BIOS
+
+    [PSCustomObject]@{
+
+        ComputerName = $env:COMPUTERNAME
+        UserName     = $env:USERNAME
+
+        WindowsName  = $os.Caption
+        Version      = $os.Version
+        BuildNumber  = $os.BuildNumber
+
+        Manufacturer = $computer.Manufacturer
+        Model        = $computer.Model
+
+        Domain       = $computer.Domain
+
+        BIOS         = $bios.SMBIOSBIOSVersion
+
+    }
+
+}
+
+Export-ModuleMember -Function Get-SystemInfo
