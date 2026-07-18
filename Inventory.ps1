94ef8476ea495e91c949def4b9d598ea87ee618c
Amir Bahmanabadi <amirbahmanabadi@outlook.com>
2026-07-18 16:13:22 +0330

    Add TXT Report Engine
    Generate an inventory report with system information and save it to a file.

--- Inventory.ps1
+++ Inventory.ps1
@@ -2,10 +2,42 @@ Import-Module "$PSScriptRoot\Modules\SystemInfo.psm1"
 
 Clear-Host
 
-Write-Host ""
-Write-Host "IT-Inventory-Pro"
-Write-Host ""
+$ReportFolder = "$PSScriptRoot\Reports"
+
+if (!(Test-Path $ReportFolder)) {
+    New-Item -ItemType Directory -Path $ReportFolder | Out-Null
+}
+
+$Time = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+
+$ReportFile = "$ReportFolder\$env:COMPUTERNAME-$Time.txt"
 
 $Info = Get-SystemInfo
 
-$Info | Format-List
+@"
+==========================================
+        IT-Inventory-Pro
+==========================================
+
+Computer Name : $($Info.ComputerName)
+User Name     : $($Info.UserName)
+
+Windows       : $($Info.WindowsName)
+Version       : $($Info.Version)
+Build Number  : $($Info.BuildNumber)
+
+Manufacturer  : $($Info.Manufacturer)
+Model         : $($Info.Model)
+
+Domain        : $($Info.Domain)
+
+BIOS Version  : $($Info.BIOS)
+
+==========================================
+
+"@ | Out-File $ReportFile -Encoding UTF8
+
+Write-Host ""
+Write-Host "Inventory Report Created Successfully" -ForegroundColor Green
+Write-Host ""
+Write-Host $ReportFile -ForegroundColor Yellow
